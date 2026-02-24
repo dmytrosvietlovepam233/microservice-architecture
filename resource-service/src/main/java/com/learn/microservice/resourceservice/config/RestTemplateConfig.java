19a784a5038933af1e11dfd0caf05ef8a1f08489
dmytro_svietlov <dmytrosvietlovepam@gmail.com>
2026-02-24 20:30:52 +0200

    Discovery service added.

--- resource-service/src/main/java/com/learn/microservice/resourceservice/config/RestTemplateConfig.java
+++ resource-service/src/main/java/com/learn/microservice/resourceservice/config/RestTemplateConfig.java
@@ -3,10 +3,12 @@ package com.learn.microservice.resourceservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
